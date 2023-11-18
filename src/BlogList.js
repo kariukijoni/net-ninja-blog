a8ad021795d7c1780a196390320a7b047a14a8ea
kariukijoni <kariukijoni@gmail.com>
2023-11-18 19:45:59 +0300

    added custom hook

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,23 +1,14 @@
-import React from 'react'
-
-function BlogList({blogs,title,handleDelete}) {
-
-    // const blogs=props.blogs
-    // const title=props.title
-
+const BlogList = ({ blogs }) => {
     return (
-        <div className='blog-preview'>
-            <h3>{title}</h3>
-            {blogs.map((blog)=>(
-                <div className='blog-preview' key={blog.id}>
-                    <h5>{blog.title}</h5>
-                    <p>Written by {blog.author}</p>
-                    <button onClick={()=>handleDelete(blog.id)}>Delete Blog</button>
-                </div>
-            ))}
-
-        </div>
-    )
-}
-
-export default BlogList
+      <div className="blog-list">
+        {blogs.map(blog => (
+          <div className="blog-preview" key={blog.id} >
+            <h2>{ blog.title }</h2>
+            <p>Written by { blog.author }</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
+   
+  export default BlogList;
